71e67a3ff7035d0de4a2721f60be1ee2ad4340e0
Vighnesh V <109282278+vvldn@users.noreply.github.com>
2023-02-25 15:43:53 +0530

    connect wallet button (#2)

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 import "./navbar.css";
+import connectWallet from "../../utils/wallet";
+
 const Navbar = () => {
+  async function attemptConnectWallet() {
+    console.log('Tryna connect Wallet...');
+    await connectWallet();
+    console.log('Voila');
+  }
   return (
     <div className="mainNav">
       <div className="navLeftDiv">
@@ -9,6 +16,7 @@ const Navbar = () => {
         <div>Register Sale</div>
       </div>
       <div className="navProfileDiv">Profile</div>
+      <button onClick={ () => attemptConnectWallet() }> Connect Wallet</button>
     </div>
   );
 };
