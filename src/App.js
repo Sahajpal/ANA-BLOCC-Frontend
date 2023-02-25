8e180914fa9e00c66bb786245cee97b9c23dd608
Danish Ahmed <danish.mohammed@eukleiatech.com>
2023-02-25 12:09:07 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import MediumButton from "./components/Button/Medium/MediumButton";
 import Card from "./components/Card/Card";
+import Sidebar from "./components/SideBar/Sidebar";
 import Dashboard from "./pages/dashboard/Dashboard";
 import SignIn from "./pages/signin/SignIn";
 
 function App() {
-  return (
-    <div>
-      {/* <Routes>
-        <Route path="" element={<SignIn />}></Route>
-        <Route path="/home" element={<Dashboard />}></Route>
-      </Routes> */}
-      <Card />
-    </div>
-  );
+  return <div className="app"></div>;
 }
 
 export default App;
