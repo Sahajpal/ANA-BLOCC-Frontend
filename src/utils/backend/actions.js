572cd92fd8466b2d1da9462d07c1a75761ca9530
Vighnesh V <vighnesh.v@myhq.in>
2023-02-26 01:03:14 +0530

    added all the utilities

--- src/utils/backend/actions.js
+++ src/utils/backend/actions.js
@@ -4,16 +4,6 @@ const registerWallet = async (walletAddress) => {
     fetchUtil('/users/wallet', { body: { address: walletAddress } });
 }
 
-const initiateSale = async (ownershipId, buyerId, transactionHash) => {
-    console.error('Yet to be implemented');
-}
-
-const initiateSaleFromAdmin = async () => {
-    console.error('Yet to be implemented');
-}
-
 export {
     registerWallet,
-    initiateSale,
-    initiateSaleFromAdmin,
 };
--- src/utils/userutils.js
+++ src/utils/userutils.js
@@ -1,60 +1,160 @@
 import getConnectedContract from './wallet';
 import { getOwnershipInfo } from './backend/ownership';
 import { getUserInfo } from './backend/user';
-import { initiateSale, initiateSaleFromAdmin } from './backend/actions';
+import fetchUtil from './backend/utils';
 
-async function initiateSaleFromAdmin(pixels, buyer) {
-    const contract = await getConnectedContract();
-    const result = await contract.initiateSaleFromAdmin(pixels, buyer, {
-        gasLimit: 2100000,
-        gasPrice: 8000000000,
-    });
-    initiateSaleFromAdmin(result.hash);
-}
+const errorToMessageMap = {
+    'E0' : 'Only admins are allowed to perform this action',
+    'E1' : 'Only property owners can perform this action',
+    'E2' : 'Only approved buyer can perform this action',
+    'E3' : 'This action cannot be performed at this stage of the transaction',
+    'E5' : 'You cannot be the buyer of your own property',
+    'E0A' : 'You are not allowed to do this action.'
+};
 
-async function initiateSale(ownershipId, buyerId) {
+// async function initiateSaleFromAdmin(pixels, buyer) {
+//     const contract = await getConnectedContract();
+//     const result = await contract.initiateSaleFromAdmin(pixels, buyer, {
+//         gasLimit: 2100000,
+//         gasPrice: 8000000000,
+//     });
+//     initiateSaleFromAdmin(result.hash);
+// }
+
+async function initiateSaleUtil(ownershipId, buyerId) {
     const ownershipPromise = getOwnershipInfo(ownershipId);
     const buyerPromise = getUserInfo(buyerId);
     const contractPromise = getConnectedContract();
     const [ownership, buyer, contract] = await Promise.all([ownershipPromise, buyerPromise, contractPromise]);
     const { ownershipId } = ownership;
     const { walletAddress: buyerAddress } = buyer;
-    const result = await contract.initiateSale(ownershipId, buyerAddress, {
-        gasLimit: 2100000,
-        gasPrice: 8000000000,
-    });
-    initiateSale(ownershipId, buyerId, result.hash);
+    let result;
+    try{
+        result = await contract.initiateSale(ownershipId, buyerAddress, {
+            gasLimit: 2100000,
+            gasPrice: 8000000000,
+        });
+    } catch(err) {
+        const { reason } = err;
+        const processedReason = reason.replace('execution reverted: ','');
+        return { success: false, reason: `Error in initiating sale : ${errorToMessageMap[processedReason]}` };
+    }
+    fetchUtil.post('', { body: ownershipId, buyerId, transactionHash: result.hash })
+    return { success: true };
 }
 
-async function acceptSale(ownershipId) {
-    
+async function acceptSaleUtil(ownershipId) {
+    const contract = await getConnectedContract();
+    let result;
+    try {
+        result = await contract.acceptSale(ownershipId);
+    } catch(err) {
+        const { reason } = err;
+        const processedReason = reason.replace('execution reverted: ','');
+        return { success: false, reason: `Error in accepting sale. ${errorToMessageMap[processedReason]}` };
+    }
+    fetchUtil.post('', { body: { ownershipId, transactionHash: result.hash }});
+    return { success: true };
 }
 
-async function uploadDocuments() {
+async function uploadDocumentsUtil() {
     // todo Kartikey
 }
 
-async function approveDocuments() {
-    // from admin
+async function approveDocumentsUtil(ownershipId) {
+    const contract = await getConnectedContract();
+    let result;
+    try {
+        result = await contract.approveDocuments(ownershipId);
+    } catch(err) {
+        const { reason } = err;
+        const processedReason = reason.replace('execution reverted: ','');
+        return { success: false, reason: `Error in approving documents. ${errorToMessageMap[processedReason]}` };
+    }
+    fetchUtil.post('', { body: { ownershipId, transactionHash: result.hash }});
+    return { success: true };
 }
 
-async function rejectDocuments() {
-    // from admin
+async function rejectDocumentsUtil(ownershipId) {
+    const contract = await getConnectedContract();
+    let result;
+    try {
+        result = await contract.rejectDocuments(ownershipId);
+    } catch(err) {
+        const { reason } = err;
+        const processedReason = reason.replace('execution reverted: ','');
+        return { success: false, reason: `Error in rejecting documents. ${errorToMessageMap[processedReason]}` };
+    }
+    fetchUtil.post('', { body: { ownershipId, transactionHash: result.hash }});
+    return { success: true };
 }
 
-async function initiatePayment() {
-
+async function initiatePaymentUtil(ownershipId) {
+    const contract = await getConnectedContract();
+    let result;
+    try {
+        result = await contract.initiatePayment(ownershipId);
+    } catch(err) {
+        const { reason } = err;
+        const processedReason = reason.replace('execution reverted: ','');
+        return { success: false, reason: `Error in initiating payments. ${errorToMessageMap[processedReason]}` };
+    }
+    fetchUtil.post('', { body: { ownershipId, transactionHash: result.hash }});
+    return { success: true };
 }
 
-async function acknowledgePayment() {
-
+async function acknowledgePaymentUtil(ownershipId) {
+    const contract = await getConnectedContract();
+    let result;
+    try {
+        result = await contract.acknowledgePayment(ownershipId);
+    } catch(err) {
+        const { reason } = err;
+        const processedReason = reason.replace('execution reverted: ','');
+        return { success: false, reason: `Error in acknowledging payments. ${errorToMessageMap[processedReason]}` };
+    }
+    fetchUtil.post('', { body: { ownershipId, transactionHash: result.hash }});
+    return { success: true };
 }
 
-async function closeSale() {
-    // from admin
+async function approveDocumentsAndGetId(hashArray, buyerAddress, contract) {
+    const contract = await getConnectedContract();
+    const result = await contract.approveDocumentsAndMarkSale(hashArray, buyerAddress);
+    return new Promise((resolve, reject) => {
+        contract.on('CloseSale', res => {
+            resolve({ hash: result.hash, ownershipId: res.toNumber() });
+        });
+    });
+}
+async function closeSaleUtil(ownershipId) {
+    const ownershipPromise = getOwnershipInfo(ownershipId);
+    const [contract, ownership] = await Promise.all([contractPromise, ownershipPromise]);
+    const buyer = await getUserInfo(ownership.buyerId);
+    let result;
+    try {
+        const hashArray = _.map(ownership.property.pixels, pixel => pixel.hash);
+        result = await approveDocumentsAndGetId(hashArray, buyer.walletAddress, contract);
+    } catch(err) {
+        const { reason } = err;
+        const processedReason = reason.replace('execution reverted: ','');
+        return { success: false, reason: `Error in closing sale. ${errorToMessageMap[processedReason]}` };
+    }
+    fetchUtil.post('', { body: { transactionHash: result.hash, newOwnershipId: result.ownershipId, oldOwnershipId: ownershipId } });
 }
 
-async function cancelSale() {}
+async function cancelSaleUtil(ownershipId) {
+    const contract = await getConnectedContract();
+    let result;
+    try {
+        result = await contract.cancelSale(ownershipId);
+    } catch(err) {
+        const { reason } = err;
+        const processedReason = reason.replace('execution reverted: ','');
+        return { success: false, reason: `Error in cancelling sales. ${errorToMessageMap[processedReason]}` };
+    }
+    fetchUtil.post('', { body: { ownershipId, transactionHash: result.hash }});
+    return { success: true };
+}
 
 async function readSale() {
     const contract = await getConnectedContract();
@@ -63,12 +163,13 @@ async function readSale() {
 }
 
 export {
-    initiateSale,
-    initiateSaleFromAdmin,
-    acceptSale,
-    uploadDocuments,
-    initiatePayment,
-    acknowledgePayment,
-    cancelSale,
-    readSale,
+    initiateSaleUtil,
+    acceptSaleUtil,
+    uploadDocumentsUtil,
+    approveDocumentsUtil,
+    rejectDocumentsUtil,
+    initiatePaymentUtil,
+    acknowledgePaymentUtil,
+    cancelSaleUtil,
+    closeSaleUtil,
 };
